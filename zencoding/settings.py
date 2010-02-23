69004bc59a8cd938c07f33bbc96bf9012ab990ab
mcrittenden <mcrittenden@freshbox.(none)>
2010-02-23 08:12:35 -0500

    move meta content type above title (thanks Kilian) and use corrent \n line breaks

--- zencoding/settings.py
+++ zencoding/settings.py
@@ -508,8 +508,8 @@ zen_settings = {
 			'html:4t': '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n' +
 					'<html lang="${lang}">\n' +
 					'<head>\n' +
-					'	<title></title>\n' +
 					'	<meta http-equiv="Content-Type" content="text/html;charset=${charset}">\n' +
+					'	<title></title>\n' +
 					'</head>\n' +
 					'<body>\n\t${child}|\n</body>\n' +
 					'</html>',
@@ -517,8 +517,8 @@ zen_settings = {
 			'html:4s': '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">\n' +
 					'<html lang="${lang}">\n' +
 					'<head>\n' +
-					'	<title></title>\n' +
 					'	<meta http-equiv="Content-Type" content="text/html;charset=${charset}">\n' +
+					'	<title></title>\n' +
 					'</head>\n' +
 					'<body>\n\t${child}|\n</body>\n' +
 					'</html>',
@@ -526,8 +526,8 @@ zen_settings = {
 			'html:xt': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n' +
 					'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="${lang}">\n' +
 					'<head>\n' +
-					'	<title></title>\n' +
 					'	<meta http-equiv="Content-Type" content="text/html;charset=${charset}" />\n' +
+					'	<title></title>\n' +
 					'</head>\n' +
 					'<body>\n\t${child}|\n</body>\n' +
 					'</html>',
@@ -535,8 +535,8 @@ zen_settings = {
 			'html:xs': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">\n' +
 					'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="${lang}">\n' +
 					'<head>\n' +
-					'	<title></title>\n' +
 					'	<meta http-equiv="Content-Type" content="text/html;charset=${charset}" />\n' +
+					'	<title></title>\n' +
 					'</head>\n' +
 					'<body>\n\t${child}|\n</body>\n' +
 					'</html>',
@@ -544,8 +544,8 @@ zen_settings = {
 			'html:xxs': '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">\n' +
 					'<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="${lang}">\n' +
 					'<head>\n' +
-					'	<title></title>\n' +
 					'	<meta http-equiv="Content-Type" content="text/html;charset=${charset}" />\n' +
+					'	<title></title>\n' +
 					'</head>\n' +
 					'<body>\n\t${child}|\n</body>\n' +
 					'</html>',
@@ -553,8 +553,8 @@ zen_settings = {
 			'html:5': '<!DOCTYPE HTML>\n' +
 					'<html lang="${locale}">\n' +
 					'<head>\n' +
-					'	<title></title>\n' +
 					'	<meta charset="${charset}">\n' +
+					'	<title></title>\n' +
 					'</head>\n' +
 					'<body>\n\t${child}|\n</body>\n' +
 					'</html>'
@@ -671,7 +671,7 @@ zen_settings = {
 			'det': '<details></details>',
 			'cmd': '<command></command>',
 
-#			expandos
+			# expandos
 			'ol+': 'ol>li',
 			'ul+': 'ul>li',
 			'dl+': 'dl>dt+dd',
@@ -714,7 +714,7 @@ zen_settings = {
 			'each': '<xsl:for-each select=""></xsl:for-each>',
 			'ap': '<xsl:apply-templates select="" mode=""/>',
 
-#			expandos
+			# expandos
 			'choose+': 'xsl:choose>xsl:when+xsl:otherwise'
 		}
 	}

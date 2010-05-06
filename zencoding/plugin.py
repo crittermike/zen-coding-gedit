4589ee6a9c22f5fdd13d915668b13252174872a4
fmarcia <franck.marcia@gmail.com>
2010-05-06 09:42:46 +0200

    adjust menu label

--- zencoding/plugin.py
+++ zencoding/plugin.py
@@ -42,19 +42,19 @@ class ZenCodingPlugin(gedit.Plugin):
 
     def activate(self, window):
         actions = [
-          ('ZenCodingMenuAction',     None, '_Zen Coding',            None,            "Zen Coding framework",                                None),
-          ('ZenCodingExpandAction',   None, '_Expand Zen Code',       '<Ctrl>E',        "Expand Zen code to raw HTML/CSS",                     self.expand_abbreviation),
-          ('ZenCodingWrapAction',     None, '_Wrap with Zen Code...', '<Ctrl><Shift>E', "Wrap selection with HTML/CSS expanded from Zen code", self.wrap_with_abbreviation),
-          ('ZenCodingInwardAction',   None, 'Balance tag _inward',    '<Ctrl><Alt>I',   "Select inner tag's content",                          self.match_pair_inward),
-          ('ZenCodingOutwardAction',  None, 'Balance tag _outward',   '<Ctrl><Alt>O',   "Select outer tag's content",                          self.match_pair_outward),
-          ('ZenCodingMergeAction',    None, '_Merge lines',           '<Ctrl><Alt>M',   "Merge all lines of the current selection",            self.merge_lines),
-          ('ZenCodingPrevAction',     None, '_Previous edit point',   '<Alt>Left',      "Place the cursor at the previous edit point",         self.prev_edit_point),
-          ('ZenCodingNextAction',     None, '_Next edit point',       '<Alt>Right',     "Place the cursor at the next edit point",             self.next_edit_point),
-          ('ZenCodingSizeAction',     None, 'Update image _size',     '<Ctrl><Alt>S',   "Update image size tag from file",                     self.update_image_size),
-          ('ZenCodingRemoveAction',   None, '_Remove tag',            '<Ctrl><Alt>R',   "Remove a tag",                                        self.remove_tag),
-          ('ZenCodingSplitAction',    None, 'Split or _join tag',     '<Ctrl><Alt>J',   "Toggle between single and double tag",                self.split_join_tag),
-          ('ZenCodingCommentAction',  None, 'Toggle _comment',        '<Ctrl><Alt>C',   "Toggle an XML or HTML comment",                       self.toggle_comment),
-          ('ZenCodingSettingsAction', None, 'E_dit settings...',      None,            "Customize snippets and abbreviations",                self.edit_settings)
+          ('ZenCodingMenuAction',     None, '_Zen Coding',                None,            "Zen Coding tools",                            None),
+          ('ZenCodingExpandAction',   None, '_Expand abbreviation',       '<Ctrl>E',        "Expand abbreviation to raw HTML/CSS",         self.expand_abbreviation),
+          ('ZenCodingWrapAction',     None, '_Wrap with abbreviation...', '<Ctrl><Shift>E', "Wrap with code expanded from abbreviation",   self.wrap_with_abbreviation),
+          ('ZenCodingInwardAction',   None, 'Balance tag _inward',        '<Ctrl><Alt>I',   "Select inner tag's content",                  self.match_pair_inward),
+          ('ZenCodingOutwardAction',  None, 'Balance tag _outward',       '<Ctrl><Alt>O',   "Select outer tag's content",                  self.match_pair_outward),
+          ('ZenCodingMergeAction',    None, '_Merge lines',               '<Ctrl><Alt>M',   "Merge all lines of the current selection",    self.merge_lines),
+          ('ZenCodingPrevAction',     None, '_Previous edit point',       '<Alt>Left',      "Place the cursor at the previous edit point", self.prev_edit_point),
+          ('ZenCodingNextAction',     None, '_Next edit point',           '<Alt>Right',     "Place the cursor at the next edit point",     self.next_edit_point),
+          ('ZenCodingSizeAction',     None, 'Update image _size',         '<Ctrl><Alt>S',   "Update image size tag from file",             self.update_image_size),
+          ('ZenCodingRemoveAction',   None, '_Remove tag',                '<Ctrl><Alt>R',   "Remove a tag",                                self.remove_tag),
+          ('ZenCodingSplitAction',    None, 'Split or _join tag',         '<Ctrl><Alt>J',   "Toggle between single and double tag",        self.split_join_tag),
+          ('ZenCodingCommentAction',  None, 'Toggle _comment',            '<Ctrl><Alt>C',   "Toggle an XML or HTML comment",               self.toggle_comment),
+          ('ZenCodingSettingsAction', None, 'E_dit settings...',          None,            "Customize snippets and abbreviations",        self.edit_settings)
         ]
         windowdata = dict()
         window.set_data("ZenCodingPluginDataKey", windowdata)

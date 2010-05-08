eb64fc24442268ebb5df8eafa9794b29dd736ef5
fmarcia <franck.marcia@gmail.com>
2010-05-08 19:19:11 +0200

    no more output for install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@
 #
 
 sudo cp zencoding.png /usr/share/icons/hicolor/16x16/apps
-sudo gtk-update-icon-cache /usr/share/icons/hicolor
-
+sudo gtk-update-icon-cache /usr/share/icons/hicolor > /dev/null 2>&1
 mkdir -p ~/.gnome2/gedit/plugins
 cp zencoding.gedit-plugin ~/.gnome2/gedit/plugins
 cp -r zencoding ~/.gnome2/gedit/plugins

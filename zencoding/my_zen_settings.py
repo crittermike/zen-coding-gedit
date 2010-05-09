afdde10f151ef22d1b225a7a69dea98d5335e57b
Mike Crittenden <mikecrittenden@gmail.com>
2010-05-09 10:05:07 -0400

    change example setting

--- zencoding/my_zen_settings.py
+++ zencoding/my_zen_settings.py
@@ -2,7 +2,7 @@ my_zen_settings = {
 	'html': {
 		'abbreviations': {
 			'jq': '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>',
-			'demo': '<div id="demo"></div>'
+			'example': '<div id="example"></div>'
 		}
 	}
 }
